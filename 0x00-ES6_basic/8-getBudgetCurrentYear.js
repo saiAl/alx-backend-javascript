2a4cb2b01ea69d87f48efe98deda59a3dd46a488
sai <ojiip95@gmail.com>
2024-07-22 19:48:51 +0200

    refactor: 0x00 task 8, return an object

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -4,15 +4,16 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  // const budget = {};
+  const budget = {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+
+  };
 
   // budget[`income-${getCurrentYear()}`] = income;
   // budget[`gdp-${getCurrentYear()}`] = gdp;
   // budget[`capita-${getCurrentYear()}`] = capita;
 
-  return {
-    [`income-${getCurrentYear()}`]: income,
-    [`gdp-${getCurrentYear()}`]: gdp,
-    [`capita-${getCurrentYear()}`]: capita,
-  };
+  return budget;
 }
