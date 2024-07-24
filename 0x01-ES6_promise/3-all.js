cc750181cd6e389e972be8b292bbf7e02e594e52
sai <ojiip95@gmail.com>
2024-07-24 21:20:43 +0200

    fix: task 3, fix checker errors

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,7 +2,6 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()]).then((values) => {
-    const string = `${values[0].body} ${values[1].firstName} ${values[1].lastName}`;
-    console.log(string);
-  }).catch(Error('Signup system offline'));
+    console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+  }).catch(Error('Signup system offline')); 
 }
