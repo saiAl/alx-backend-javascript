fddf490dc2a2b6ec83fa760f189db5768ac69403
sai <ojiip95@gmail.com>
2024-07-24 11:38:20 +0200

    build: 0x01 task 1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,16 @@
+export default function getFullResponseFromAPI(success) {
+  const promise = new Promise((resolve, reject) => {
+    if (success) {
+      resolve(
+        {
+          status: 200,
+          body: 'Success'
+        }
+      );
+    } else {
+      reject("The fake API is not working currently");
+    }
+  });
+
+  return promise
+}
