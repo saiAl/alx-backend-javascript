cc0f40520a73f2293386812380ac419ea1e4e91d
sai <ojiip95@gmail.com>
2024-07-24 23:46:05 +0200

    fix: task 6, the object returned

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,13 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((result) => result);
+    .then((values) => {
+      const obj = values.map((item) => {
+        if (item.status === 'fulfilled') {
+          return item;
+        }
+        return { status: item.status, value: String(item.reason) };
+      });
+      return obj;
+    });
 }
