2b0b21fe49cb59e8c057708a351c4000d890c527
sai <ojiip95@gmail.com>
2024-07-24 19:02:44 +0200

    feat: task 3, catch errors

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,5 +4,5 @@ export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()]).then((values) => {
     const string = `${values[0].body} ${values[1].firstName} ${values[1].lastName}`;
     console.log(string);
-  });
+  }).catch(Error('Signup system offline'));
 }
