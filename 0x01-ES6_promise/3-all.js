29029ee1c4353a9987a87a5d60058d0aaa5b5316
sai <ojiip95@gmail.com>
2024-07-24 21:23:48 +0200

    fix: task 3, eslint errors

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,5 +3,5 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()]).then((values) => {
     console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
-  }).catch(Error('Signup system offline')); 
+  }).catch('Signup system offline');
 }
