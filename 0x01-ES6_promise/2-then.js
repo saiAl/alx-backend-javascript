22aab1f710fe8c8a8e6f76ee6453750cb3397e32
sai <ojiip95@gmail.com>
2024-07-24 12:27:37 +0200

    fix: 0x01 task 2, return the promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({
+  return promise.then(() => ({
     status: 200,
     body: 'success',
   })).catch(() => new Error()).finally(console.log('Got a response from the API'));
-  return promise;
 }
