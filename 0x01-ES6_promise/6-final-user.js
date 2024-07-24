ec4094872e519d34aefcead2ad90f32f6e4bfc94
sai <ojiip95@gmail.com>
2024-07-24 21:38:09 +0200

    fix: task 6, delete then so the function just shows the status

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,4 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((v) => console.log(v));
 }
