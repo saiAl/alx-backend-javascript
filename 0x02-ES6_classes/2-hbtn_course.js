51555e0cab3ba40693532fefc743209cfd40ef58
sai <ojiip95@gmail.com>
2024-07-25 19:06:01 +0200

    build: task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,43 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+
+    if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+
+    if (!Array.isArray(students)) {
+      throw TypeError('Studnets must be an array');
+    }
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    this._length = value;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    this._students = value;
+  }
+}
