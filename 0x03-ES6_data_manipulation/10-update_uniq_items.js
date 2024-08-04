913ddbec45b8af3a513bef9433673cfa5c685494
sai <ojiip95@gmail.com>
2024-08-04 16:46:54 +0200

    refactor: handle not a map edge case

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,7 @@
 export default function updateUniqueItems(arg) {
+  if (arg.constructor.name != 'Map') {
+    throw new Error("Cannot process")
+  }
   for (const entity of arg) {
     const key = arg.get(entity[0]);
     if (key === 1) {
