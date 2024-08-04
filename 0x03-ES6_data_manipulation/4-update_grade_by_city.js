578e62eebde33df6f05511db308b64bf60027c43
sai <ojiip95@gmail.com>
2024-08-04 15:46:39 +0200

    fix: eslint errors

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -3,10 +3,10 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   filtered.map((student) => {
     for (let idx = 0; idx < newGrades.length; idx += 1) {
       if (student.id === newGrades[idx].studentId) {
-        student.grade = newGrades[idx].grade;
+        student.grade = newGrades[idx].grade; /* eslint no-prototype-builtins: off */
       } else if (
-        student.id !== newGrades[idx].studentId &&
-        !student.hasOwnProperty('grade')
+        student.id !== newGrades[idx].studentId
+        && !student.hasOwnProperty('grade') /* eslint no-param-reassign: off */
       ) {
         student.grade = 'N/A';
       }
