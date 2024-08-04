6efb084ac86b944c3910c77171ac15f6dcf5282c
sai <ojiip95@gmail.com>
2024-08-05 00:05:35 +0200

    build: task 6

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -40,9 +40,35 @@ function createEmployee(salary: number | string): Director | Teacher {
   }
   return new Director();
 }
-
+/* 
 console.log(createEmployee(200));
-Teacher;
+// Teacher;
 console.log(createEmployee(1000));
-Director;
+// Director;
 console.log(createEmployee("$500"));
+ */
+// task 6
+
+/* function isDirector(employee: Director | Teacher): Director {
+  if (employee instanceof Director) {
+    return new Director()
+} */
+
+function executeWork(employee: Director | Teacher) {
+  const isDirector = (emp: Director | Teacher) => {
+    emp instanceof Director ? new Director() : new Teacher();
+    return emp;
+  };
+  const typeOf = isDirector(employee);
+
+  if (typeOf instanceof Director) {
+    console.log(typeOf.workDirectorTasks());
+  } else {
+    console.log(typeOf.workTeacherTasks());
+  }
+}
+
+executeWork(createEmployee(200));
+// Getting to work
+executeWork(createEmployee(1000));
+// Getting to director tasks
