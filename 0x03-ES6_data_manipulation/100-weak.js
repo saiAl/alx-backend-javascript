81ee5117eeb5ac2bb9e3aa8335fea461bd1622a5
sai <ojiip95@gmail.com>
2024-08-04 17:19:38 +0200

    build: task 100

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,12 @@
+const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  const count = weakMap.get(endpoint) || 0;
+  weakMap.set(endpoint, count + 1);
+
+  if (count + 1 >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
+
+export { weakMap };
