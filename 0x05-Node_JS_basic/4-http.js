c750aab959e5ea33d596694b99e5aefa96b5401b
sai <ojiip95@gmail.com>
2024-10-01 16:23:45 +0200

    refactor: exports the code to reuse across source code

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,15 @@
 const http = require('http'); // Import the Node.js HTTP module
 
 // Create an HTTP server that responds to every request with "Hello Holberton School!"
-const server = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   // Set the HTTP response status code to 200 (OK)
-  res.writeHead(200);
-
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   // End the response with the message "Hello Holberton School!"
   res.end('Hello Holberton School!');
 });
 
 // Start the server and have it listen on port 1245
-server.listen(1245);
+app.listen(1245, () => console.log('Server is running'));
+
+module.exports = app;
