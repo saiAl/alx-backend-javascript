57e98947c96a8b392281e78df6837f8d407f16a1
sai <ojiip95@gmail.com>
2024-10-06 12:15:17 +0000

    build: task 6

---
+++ 0x05-Node_JS_basic/6-http.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(1245, () => {
+  console.log('server is running on port 1245');
+});
